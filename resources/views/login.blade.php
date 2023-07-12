ae08057b65616e6cbccf6661e72f2ec6b91d7373
Andrew Yaremenko <esdesuagk@gmail.com>
2023-07-12 12:39:12 +0300

    remember user logic

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,19 +9,24 @@
                         @csrf
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
-                                id="email" placeholder="email" value="{{ old('email') }}" required autofocus>
+                            <input type="email" name="email"
+                                class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="email"
+                                placeholder="email" value="{{ old('email') }}" required autofocus>
                             @error('email')
                                 <p class="text-danger">{{ $message }}</p>
                             @enderror
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
-                                id="password" placeholder="password" required>
+                            <input type="password" name="password"
+                                class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" id="password"
+                                placeholder="password" required>
                             @error('password')
                                 <p class="text-danger">{{ $message }}</p>
                             @enderror
+                        </div>
+                        <div class="mb-3">
+                            <input type="checkbox" name="remember" id="remember"> Remember me
                         </div>
                         <div class="d-grid">
                             <button type="submit" class="btn btn-primary">Login</button>
